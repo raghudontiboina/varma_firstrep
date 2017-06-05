bb6016d1d740b501a22db727df5472475038f0d4
raghudontiboina <raghu.dontiboina@gmail.com>
2017-06-04 21:30:00 -0700

    midnight

---
+++ now.sh
@@ -0,0 +1,19 @@
+NOW="$(date +'%B %d, %Y')"
+echo $NOW
+RED="\033[1;31m"
+echo $RED
+GREEN="\033[0;32m"
+echo $GREEN
+YELLOW="\033[1;33m"
+echo $YELLOW
+BLUE="\033[1;34m"
+echo $BLUE
+PURPLE="\033[1;35m"
+echo $PURPLE
+CYAN="\033[1;36m"
+echo $CYAN
+WHITE="\033[1;37m"
+echo $WHITE
+RESET="\033[0m"
+echo $RESET
+

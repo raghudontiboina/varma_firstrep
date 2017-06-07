10806e174ec40541b85e37ace5063d77c6374bbc
raghudontiboina <raghu.dontiboina@gmail.com>
2017-06-06 21:33:19 -0700

    makefile

--- hellomake.c
+++ hellomake.c
@@ -7,4 +7,6 @@ printf("this is hellomake.c\n");
 printf("this is hellomake.c added line\n");
 printf("this is second line in hellomake.c\n");
 printf("this is third line in hellomake.c\n");
+printf("to check in git\n");
+
 }
